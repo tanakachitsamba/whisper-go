48debab99a3932b24fb875f6d96967d65ef8d108
Tanaka <alongthejourneyahead@gmail.com>
2023-04-20 13:06:24 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module test
+
+go 1.18
+
+require github.com/sashabaranov/go-openai v1.5.7
+
+require github.com/joho/godotenv v1.5.1
---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+	"sync"
+
+	"github.com/joho/godotenv"
+	openai "github.com/sashabaranov/go-openai"
+)
+
+func transcribeFile(c *openai.Client, ctx context.Context, audioFile string, wg *sync.WaitGroup, transcriptions *[]string) {
+	defer wg.Done()
+
+	req := openai.AudioRequest{
+		Model:    openai.Whisper1,
+		FilePath: audioFile,
+	}
+	resp, err := c.CreateTranscription(ctx, req)
+	if err != nil {
+		fmt.Printf("Transcription error for %s: %v\n", audioFile, err)
+		return
+	}
+
+	*transcriptions = append(*transcriptions, resp.Text)
+}
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("error loading .env file:", err)
+	}
+	c := openai.NewClient(os.Getenv("OPENAI_KEY"))
+	ctx := context.Background()
+
+	// Define the audio files directory and the audio format
+	audioDir := "audios/"
+	audioFormat := ".mp3"
+
+	// Read the audio files from the directory
+	files, err := ioutil.ReadDir(audioDir)
+	if err != nil {
+		fmt.Printf("Error reading audio directory: %v\n", err)
+		return
+	}
+
+	// Filter audio files based on the audio format
+	var audioFiles []string
+	for _, file := range files {
+		if strings.HasSuffix(file.Name(), audioFormat) {
+			audioFiles = append(audioFiles, audioDir+file.Name())
+		}
+	}
+
+	// Process the audio files and store their transcriptions in a slice
+	var transcriptions []string
+	var wg sync.WaitGroup
+	for _, audioFile := range audioFiles {
+		wg.Add(1)
+		go transcribeFile(c, ctx, audioFile, &wg, &transcriptions)
+	}
+
+	wg.Wait()
+
+	// Print the transcriptions
+	for i, transcription := range transcriptions {
+		fmt.Printf("Transcription %d: %s\n", i+1, transcription)
+	}
+}
